37aafd32d747429056b3f4ebdb5ff22bb9aa6f92
nero <ogudenero@gmail.com>
2023-02-12 05:14:06 +0000

    ManageAddressPage

---
+++ src/main/java/pageObject/ManageAddressPage.java
@@ -0,0 +1,30 @@
+package pageObject;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import base.BasePage;
+
+public class ManageAddressPage extends BasePage{
+	
+	public WebDriver driver;
+	
+	By addNewAddressLink = By.cssSelector(".d-flex.justify-content-end [data-toggle]");
+	By useAddressBtn = By.cssSelector("main [tabindex='-1']:nth-child(15) .select-address");
+
+	public ManageAddressPage() throws IOException {
+		super();	
+	}
+	public WebElement getAddNewAddressLink() throws IOException {
+		this.driver = getDriver();
+		return driver.findElement(addNewAddressLink);
+	}
+	public WebElement getUseAddressBtn() throws IOException {
+		this.driver = getDriver();
+		return driver.findElement(useAddressBtn);
+	}
+
+}
